9709bc685c338f263bb2bed1f561fbc56d3ff6d2
Kwak Byoung Min <rovin1273@gmail.com>
2025-07-22 15:28:31 +0900

    comment: 티켓팅 이벤트 로직 주석 및 티켓팅 에러 코드 추가

--- src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/TicketingService.java
+++ src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/TicketingService.java
@@ -2,19 +2,19 @@ package inu.codin.codinticketingapi.domain.ticketing.service;
 
 import inu.codin.codinticketingapi.domain.admin.entity.Event;
 import inu.codin.codinticketingapi.domain.image.service.ImageService;
-import inu.codin.codinticketingapi.domain.ticketing.dto.event.StockDecrementRequest;
 import inu.codin.codinticketingapi.domain.ticketing.dto.response.ParticipationCreateResponse;
 import inu.codin.codinticketingapi.domain.ticketing.entity.Participation;
+import inu.codin.codinticketingapi.domain.ticketing.entity.ParticipationStatus;
 import inu.codin.codinticketingapi.domain.ticketing.entity.Profile;
+import inu.codin.codinticketingapi.domain.ticketing.entity.Stock;
 import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingErrorCode;
 import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingException;
 import inu.codin.codinticketingapi.domain.ticketing.repository.EventRepository;
 import inu.codin.codinticketingapi.domain.ticketing.repository.ParticipationRepository;
 import inu.codin.codinticketingapi.domain.ticketing.repository.ProfileRepository;
+import inu.codin.codinticketingapi.domain.ticketing.repository.StockRepository;
 import inu.codin.codinticketingapi.domain.user.service.UserClientService;
-import inu.codin.codinticketingapi.security.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,11 +26,17 @@ public class TicketingService {
     private final EventRepository eventRepository;
     private final ProfileRepository profileRepository;
     private final ParticipationRepository participationRepository;
+    private final StockRepository stockRepository;
 
-    private final ApplicationEventPublisher eventPublisher;
+//    private final ApplicationEventPublisher eventPublisher;
     private final UserClientService userClientService;
     private final ImageService imageService;
 
+    /**
+     * 유저 티켓팅 참여
+     * @param eventId 유저가 참여할 티켓팅 이벤트
+     * @return ParticipationCreateResponse 티켓팅 이벤트 유저 참여 정보
+     */
     @Transactional
     public ParticipationCreateResponse saveParticipation(Long eventId) {
         String userId = userClientService.fetchUser().getUserId();
@@ -39,6 +45,9 @@ public class TicketingService {
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.PROFILE_NOT_FOUND));
         Event event = eventRepository.findById(eventId)
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.EVENT_NOT_FOUND));
+        Stock stock = stockRepository.findById(eventId)
+                .orElseThrow(() -> new TicketingException(TicketingErrorCode.STOCK_NOT_FOUND));
+        // todo: EventStatus 검증
 
         // todo: int ticketNumber = Stock에서 티켓팅 번호 가져오기
         Participation participation = Participation.builder()
@@ -46,9 +55,17 @@ public class TicketingService {
                 .ticketNumber(1)
                 .profile(profile)
                 .build();
+
+        // eventPublisher.publishEvent(new StockDecrementRequest(event));
         return ParticipationCreateResponse.of(participationRepository.save(participation));
     }
 
+    /**
+     * 티켓팅 이벤트 유저 참여 상태를 수령으로 변경
+     * @param eventId 유저가 참여한 이벤트
+     * @param adminPassword 수령 상태 확인을 위한 관리자 비밀번호
+     * @param signatureImage 수령 확인 서명 이미지 파일
+     */
     @Transactional
     public void processParticipationSuccess(Long eventId, String adminPassword, MultipartFile signatureImage) {
         String userId = userClientService.fetchUser().getUserId();
@@ -57,6 +74,7 @@ public class TicketingService {
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.PROFILE_NOT_FOUND));
         Event event = eventRepository.findById(eventId)
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.EVENT_NOT_FOUND));
+        // todo: EventStatus 검증
 
         // 비밀번호 확인
         if (!adminPassword.equals(event.getEventPassword())) {
@@ -70,14 +88,17 @@ public class TicketingService {
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.PARTICIPATION_NOT_FOUND));
 
         // 수령 완료 상태로 변경
+        if (!participation.getStatus().equals(ParticipationStatus.WAITING)) {
+            throw new TicketingException(TicketingErrorCode.CANNOT_CHANGE_STATUS);
+        }
         participation.changeStatusCompleted();
         participation.setSignatureImgUrl(signatureImageUrl);
-        participationRepository.save(participation);
-
-        // 수령 정보 반영
-        eventPublisher.publishEvent(new StockDecrementRequest(event));
     }
 
+    /**
+     * 티켓팅 이벤트 유저 참여 상태를 취소로 변경
+     * @param eventId 유저가 참여한 이벤트
+     */
     @Transactional
     public void changeParticipationStatusCanceled(Long eventId) {
         String userId = userClientService.fetchUser().getUserId();
@@ -86,11 +107,15 @@ public class TicketingService {
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.PROFILE_NOT_FOUND));
         Event event = eventRepository.findById(eventId)
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.EVENT_NOT_FOUND));
+        // todo: EventStatus 검증
         Participation participation = participationRepository.findByEventAndProfile(event, profile)
                 .orElseThrow(() -> new TicketingException(TicketingErrorCode.PARTICIPATION_NOT_FOUND));
 
+        if (!participation.getStatus().equals(ParticipationStatus.WAITING)) {
+            throw new TicketingException(TicketingErrorCode.CANNOT_CHANGE_STATUS);
+        }
+        // todo: 이벤트 시간 내에 취소시 Stock 개수 증가
         participation.changeStatusCanceled();
-        participationRepository.save(participation);
     }
 
     // todo: 특정 이벤트의 잔여수량 실시간 체크 기능 (STOMP?)
