137d1c07f88e5f825fc43b1b1ad0e68ef19f6f42
Kwak Byoung Min <rovin1273@gmail.com>
2025-07-23 16:31:01 +0900

    remove: 미사용 Stock 서비스 로직 주석화

--- src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/StockService.java
+++ src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/StockService.java
@@ -1,34 +1,34 @@
-package inu.codin.codinticketingapi.domain.ticketing.service;
-
-import inu.codin.codinticketingapi.domain.admin.entity.Event;
-import inu.codin.codinticketingapi.domain.ticketing.dto.event.StockDecrementRequest;
-import inu.codin.codinticketingapi.domain.ticketing.entity.Stock;
-import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingErrorCode;
-import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingException;
-import inu.codin.codinticketingapi.domain.ticketing.repository.StockRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
-
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class StockService {
-
-    private final StockRepository stockRepository;
-
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    public void handleStockDecrementRequest(StockDecrementRequest request){
-        Event event = request.getEvent();
-        Stock stock = stockRepository.findByEvent(event)
-                .orElseThrow(() -> new TicketingException(TicketingErrorCode.STOCK_NOT_FOUND));
-
-        if (stock.decrement()) {
-            stockRepository.save(stock);
-        } else {
-            log.error("[handleStockDecrementRequest] Stock has been decremented by eventId : {}", event.getId());
-        }
-    }
-}
+//package inu.codin.codinticketingapi.domain.ticketing.service;
+//
+//import inu.codin.codinticketingapi.domain.admin.entity.Event;
+//import inu.codin.codinticketingapi.domain.ticketing.dto.event.StockDecrementRequest;
+//import inu.codin.codinticketingapi.domain.ticketing.entity.Stock;
+//import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingErrorCode;
+//import inu.codin.codinticketingapi.domain.ticketing.exception.TicketingException;
+//import inu.codin.codinticketingapi.domain.ticketing.repository.StockRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.event.TransactionPhase;
+//import org.springframework.transaction.event.TransactionalEventListener;
+//
+//@Service
+//@Slf4j
+//@RequiredArgsConstructor
+//public class StockService {
+//
+//    private final StockRepository stockRepository;
+//
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//    public void handleStockDecrementRequest(StockDecrementRequest request){
+//        Event event = request.getEvent();
+//        Stock stock = stockRepository.findByEvent(event)
+//                .orElseThrow(() -> new TicketingException(TicketingErrorCode.STOCK_NOT_FOUND));
+//
+//        if (stock.decrease()) {
+//            stockRepository.save(stock);
+//        } else {
+//            log.error("[handleStockDecrementRequest] Stock has been decremented by eventId : {}", event.getId());
+//        }
+//    }
+//}
