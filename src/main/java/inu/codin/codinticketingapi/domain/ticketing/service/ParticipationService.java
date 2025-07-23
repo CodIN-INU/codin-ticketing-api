ca549b59c49a168abec3be897bda208f29179f50
BHC-Chicken <tij05078@gmail.com>
2025-07-23 22:18:08 +0900

    refactor : 낙관적 락 -> 비관적 락으로 전환
    - 테스트 결과 재시도에서 빠르게 실패하여 성공해야하는 경우에도 실패 발생
    - 비관적 락으로 전환 후 문제 해결

--- src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/ParticipationService.java
+++ src/main/java/inu/codin/codinticketingapi/domain/ticketing/service/ParticipationService.java
@@ -1,8 +1,14 @@
 package inu.codin.codinticketingapi.domain.ticketing.service;
 
+import inu.codin.codinticketingapi.domain.admin.entity.Event;
 import inu.codin.codinticketingapi.domain.ticketing.dto.response.EventParticipationHistoryDto;
+import inu.codin.codinticketingapi.domain.ticketing.dto.response.ParticipationCreateResponse;
+import inu.codin.codinticketingapi.domain.ticketing.entity.Participation;
 import inu.codin.codinticketingapi.domain.ticketing.entity.ParticipationStatus;
+import inu.codin.codinticketingapi.domain.ticketing.entity.Stock;
 import inu.codin.codinticketingapi.domain.ticketing.repository.ParticipationRepository;
+import inu.codin.codinticketingapi.domain.user.dto.UserInfoResponse;
+import inu.codin.codinticketingapi.domain.user.service.UserClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ParticipationService {
 
+    private final TicketingService ticketingService;
+    private final UserClientService userClientService;
     private final ParticipationRepository participationRepository;
 
     @Deprecated
@@ -25,5 +33,22 @@ public class ParticipationService {
     @Transactional(readOnly = true)
     public Page<EventParticipationHistoryDto> getUserEventHistoryByCanceled(String userId, ParticipationStatus status, Pageable pageable) {
         return participationRepository.findHistoryByUserIdAndCanceled(userId, status, pageable);
+    }
+
+    @Transactional
+    public ParticipationCreateResponse saveParticipation(Event event) {
+        UserInfoResponse userInfoResponse = userClientService.fetchUser();
+        Stock stock = ticketingService.decrement(event.getId());
+
+        // 사용자 번호표
+        int ticketNumber = stock.getInitialStock() - stock.getStock();
+
+        Participation participation = Participation.builder()
+                .event(event)
+                .ticketNumber(ticketNumber)
+                .userInfoResponse(userInfoResponse)
+                .build();
+
+        return ParticipationCreateResponse.of(participationRepository.save(participation));
     }
 }
