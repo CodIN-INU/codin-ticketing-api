69f539e02db549821196be47484b1554e5d98e12
BHC-Chicken <tij05078@gmail.com>
2025-08-02 15:12:49 +0900

    refactor : 이벤트 참가자 없을 경우 빈 엑셀 다운로드로 변경

--- src/main/java/inu/codin/codinticketingapi/domain/admin/service/ExcelService.java
+++ src/main/java/inu/codin/codinticketingapi/domain/admin/service/ExcelService.java
@@ -31,8 +31,10 @@ public class ExcelService {
     private final ParticipationRepository participationRepository;
 
     private final static int ROW_START = 1;
+    private final static String NO_PARTICIPANTS = "현재 이벤트에 참가자가 존재하지 않습니다.";
     private final static String SHEET_NAME_SUFFIX = "_참가자";
-    private final static String[] HEADERS = {"사용자 ID", "이름", "학과", "학번", "경품 수령 상태", "교환권 번호"};
+    private final static String UNKNOWN = "UNKNOWN";
+    private final static String[] HEADERS = {"사용자 ID", "이름", "학과", "학번", "경품 수령 상태", "교환권 번호", "서명"};
 
     @Transactional(readOnly = true)
     public ExcelResponse getExcel(Long eventId) {
@@ -65,6 +67,17 @@ public class ExcelService {
         return eventRepository.findById(eventId).orElseThrow(() -> new TicketingException(TicketingErrorCode.EVENT_NOT_FOUND));
     }
 
+    /**
+     * 주어진 파일명을 URL 또는 HTTP 헤더에서 안전하게 사용할 수 있도록 인코딩합니다.
+     * <p>
+     * URL 인코딩은 파일명에 포함된 공백과 같은 특수 문자들이
+     * URL 또는 HTTP 헤더에서 사용될 때 발생할 수 있는 문제를 방지하기 위해 올바르게 이스케이프되도록 합니다.
+     * 기본적으로 URLEncoder는 공백을 '+'로 인코딩하지만, 이 메서드는 '+'를 '%20'으로 대체하여
+     * 파일명에 대한 표준 URL 인코딩 규칙을 따르도록 합니다.
+     *
+     * @param filename 인코딩할 원본 파일명.
+     * @return 공백이 '%20'으로 대체된 URL 인코딩된 파일명.
+     */
     private String encodeFileName(String filename) {
 
         return URLEncoder.encode(filename, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
@@ -80,25 +93,28 @@ public class ExcelService {
     }
 
     private List<Participation> getParticipation(Long eventId) {
-        List<Participation> participationList = participationRepository.findAllByEvent_Id(eventId);
 
-        if (participationList.isEmpty()) {
-            throw new TicketingException(TicketingErrorCode.PARTICIPATION_NOT_FOUND);
-        }
 
-        return participationList;
+        return participationRepository.findAllByEvent_Id(eventId);
     }
 
     private void populateDataRows(Sheet sheet, List<Participation> participationList) {
         int rowNum = ROW_START;
 
+        if (participationList.isEmpty()) {
+            Row row = sheet.createRow(rowNum);
+            row.createCell(0).setCellValue(NO_PARTICIPANTS);
+
+            return;
+        }
+
         for (Participation participation : participationList) {
             Row row = sheet.createRow(rowNum++);
             row.createCell(0).setCellValue(participation.getUserId());
             row.createCell(1).setCellValue(participation.getName());
-            row.createCell(2).setCellValue(participation.getDepartment().name());
+            row.createCell(2).setCellValue(participation.getDepartment() != null ? participation.getDepartment().name() : UNKNOWN);
             row.createCell(3).setCellValue(participation.getStudentId());
-            row.createCell(4).setCellValue(participation.getStatus().name());
+            row.createCell(4).setCellValue(participation.getStatus() != null ? participation.getStatus().name() : UNKNOWN);
             row.createCell(5).setCellValue(participation.getTicketNumber());
         }
     }
