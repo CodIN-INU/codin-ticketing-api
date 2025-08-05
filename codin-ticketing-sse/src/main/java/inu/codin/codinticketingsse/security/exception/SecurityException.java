bb5ffc5f697b2cbc309a29c03d785c5d0a66280a
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-05 13:51:08 +0900

    feat: Spring Security 추가

---
+++ codin-ticketing-sse/src/main/java/inu/codin/codinticketingsse/security/exception/SecurityException.java
@@ -0,0 +1,15 @@
+package inu.codin.codinticketingsse.security.exception;
+
+
+import inu.codin.codinticketingsse.common.exception.GlobalException;
+import lombok.Getter;
+
+@Getter
+public class SecurityException extends GlobalException {
+    private final SecurityErrorCode securityErrorCode;
+
+    public SecurityException(SecurityErrorCode errorCode) {
+        super(errorCode);
+        this.securityErrorCode = errorCode;
+    }
+}
