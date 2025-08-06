cb1eab056621370970cf0ddd6b6c004fd7a45402
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-07 02:49:10 +0900

    config: Feign, MethodSecurity, EnableJpaAuditing Config 클래스 분리
    - 미사용중인 트랜잭션 보상 EnableRetry 삭제
    - JpaConfig로 EnableJpaAuditing 분리
    - Test 환경에서 AutoConfigureMockMvc로 빈 주입 통제를 위해 SpringBootApplication에서 분리 시켜야함.

--- src/main/java/inu/codin/codinticketingapi/config/SecurityConfig.java
+++ src/main/java/inu/codin/codinticketingapi/config/SecurityConfig.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtTokenValidator jwtTokenValidator;
