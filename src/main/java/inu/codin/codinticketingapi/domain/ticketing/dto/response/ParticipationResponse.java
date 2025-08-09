7ba1601c686b974effac544ffaa0920fcb96af45
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-09 18:29:04 +0900

    fix: 티켓팅 참여 정보 Cache 데이터 조회시 JsonCreator 필요 문제 해결

--- src/main/java/inu/codin/codinticketingapi/domain/ticketing/dto/response/ParticipationResponse.java
+++ src/main/java/inu/codin/codinticketingapi/domain/ticketing/dto/response/ParticipationResponse.java
@@ -1,13 +1,18 @@
 package inu.codin.codinticketingapi.domain.ticketing.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import inu.codin.codinticketingapi.domain.ticketing.entity.Participation;
 import inu.codin.codinticketingapi.domain.ticketing.entity.ParticipationStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "티켓팅 이벤트 수령 생성 응답 DTO")
 public class ParticipationResponse {
 
@@ -19,6 +24,17 @@ public class ParticipationResponse {
 
     @Schema(description = "서명 이미지 URL", example = "https://codin-s3-bucket.s3.ap-northeast-2.amazonaws.com/signature.jpeg")
     private String signatureImgUrl;
+
+    @JsonCreator
+    public ParticipationResponse(
+            @JsonProperty("status") ParticipationStatus status,
+            @JsonProperty("ticketNumber") Integer ticketNumber,
+            @JsonProperty("signatureImgUrl") String signatureImgUrl
+    ) {
+        this.status = status;
+        this.ticketNumber = ticketNumber;
+        this.signatureImgUrl = signatureImgUrl;
+    }
 
     public static ParticipationResponse of(Participation participation) {
         return ParticipationResponse.builder()
