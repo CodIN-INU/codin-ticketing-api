6cf1bf4f5d23ebbe984205e34b671577e7597ce3
Kwak Byoung Min <rovin1273@gmail.com>
2025-08-09 23:25:47 +0900

    refactor: Timezone Asia/Seoul 설정 추가

--- src/main/java/inu/codin/codinticketingapi/CodinTicketingApiApplication.java
+++ src/main/java/inu/codin/codinticketingapi/CodinTicketingApiApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients
 public class CodinTicketingApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(CodinTicketingApiApplication.class, args);
     }
 
