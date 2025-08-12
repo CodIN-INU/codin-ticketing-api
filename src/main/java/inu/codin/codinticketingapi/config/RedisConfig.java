f76e60bd27cf4058b7b4e95afa70a12ca0f4d69a
BHC-Chicken <tij05078@gmail.com>
2025-08-12 17:49:56 +0900

    refactor : redis에서 티켓 번호 발부하도록 변경
    - 이벤트 생성 시 redis에 이벤트 번호를 키로 토큰 초기화
       - ex) 초기 물량 100일 경우 redis set 자료 구조에 1~100 숫자 추가
       - score를 삽입되는 숫자와 같게하여 오름차순 정렬
    
    - 이벤트 참가 시 popMin() 을 통해 티켓 번호 발부
    
    - 이벤트 참가 취소 시 티켓 번호 redis에 삽입

--- src/main/java/inu/codin/codinticketingapi/config/RedisConfig.java
+++ src/main/java/inu/codin/codinticketingapi/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package inu.codin.codinticketingapi.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -49,12 +52,42 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        // LocalDateTime을 타임스탬프(숫자)가 아닌 ISO-8601 형식의 문자열로 직렬화
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 2. ObjectMapper를 사용하는 Serializer 생성
+        return getStringObjectRedisTemplate(redisConnectionFactory, objectMapper);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> eventRedisTemplate (RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> pingRedisTemplate (RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    private RedisTemplate<String, Object> getStringObjectRedisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setDefaultSerializer(RedisSerializer.string());
-        redisTemplate.setEnableTransactionSupport(true);
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
         return redisTemplate;
     }
 }
