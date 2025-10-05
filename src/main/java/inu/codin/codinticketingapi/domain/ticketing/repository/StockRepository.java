214a39a42c3f0940db54062a08d5394a80af6d9a
BHC-Chicken <tij05078@gmail.com>
2025-10-06 04:07:59 +0900

    refactor : 필요없는 락 제거
    - redis에서 먼저 티켓 수량을 확인하도록 변경했습니다.
    - 재고관리는 redis를 통해 하고 있으므로 DB의 비관적 락은 필요없어져 제거하였습니다.

--- src/main/java/inu/codin/codinticketingapi/domain/ticketing/repository/StockRepository.java
+++ src/main/java/inu/codin/codinticketingapi/domain/ticketing/repository/StockRepository.java
@@ -2,9 +2,7 @@ package inu.codin.codinticketingapi.domain.ticketing.repository;
 
 import inu.codin.codinticketingapi.domain.admin.entity.Event;
 import inu.codin.codinticketingapi.domain.ticketing.entity.Stock;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,11 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
     Optional<Stock> findByEvent(Event event);
 
-
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from Stock s where s.event.id = :eventId")
     Optional<Stock> findByEvent_Id(Long eventId);
 }
