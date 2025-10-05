0044a0bc2bf9bcc916c465e64acce3db97df23fc
BHC-Chicken <tij05078@gmail.com>
2025-10-06 04:10:58 +0900

    feat : ParticipationResponse template 생성
    - redis에 ParticipationResponse 저장을 위한 template 을 생성 및 RedisParticipationService 에서 해당 template 사용하도록 수정

--- src/main/java/inu/codin/codinticketingapi/config/RedisConfig.java
+++ src/main/java/inu/codin/codinticketingapi/config/RedisConfig.java
@@ -3,6 +3,7 @@ package inu.codin.codinticketingapi.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import inu.codin.codinticketingapi.domain.ticketing.dto.response.ParticipationResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -45,9 +47,9 @@ public class RedisConfig {
         redisStandAloneConfiguration.setDatabase(REDIS_DATABASE);
 
         return new LettuceConnectionFactory(redisStandAloneConfiguration, LettuceClientConfiguration.builder()
-                    .commandTimeout(Duration.ofMillis(500)) // 명령 타임아웃
-                    .shutdownTimeout(Duration.ofMillis(100)) // 셧다운 타임아웃
-                    .build());
+                .commandTimeout(Duration.ofMillis(500)) // 명령 타임아웃
+                .shutdownTimeout(Duration.ofMillis(100)) // 셧다운 타임아웃
+                .build());
     }
 
     @Bean
@@ -62,7 +64,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> eventRedisTemplate (RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, String> eventRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -72,11 +74,30 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> pingRedisTemplate (RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, String> pingRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, ParticipationResponse> participationRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<ParticipationResponse> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, ParticipationResponse.class);
+
+        RedisTemplate<String, ParticipationResponse> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(valueSerializer); // Value serializer로 위에서 만든 것을 사용
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(valueSerializer);
 
         return redisTemplate;
     }
